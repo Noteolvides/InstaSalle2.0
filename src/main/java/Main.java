44fb464df321ab20f32b4a263d8996d984f22be4
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-12-27 16:49:20 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
+import Json.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Gson gson = new GsonBuilder().create();
+        User[] users = gson.fromJson(new FileReader("Datasets/users.json"), User[].class);
+        Nodes[] nodes = gson.fromJson(new FileReader("Datasets/nodes.json"), Nodes[].class);
+        Server[] servers = gson.fromJson(new FileReader("Datasets/servers.json"), Server[].class);
     }
 }
