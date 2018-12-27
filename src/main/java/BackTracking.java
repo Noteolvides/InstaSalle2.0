488fd36e4502dda0a73b116831d2a12ad68cd211
NeilTorrero <neil.torrero@students.salle.url.edu>
2018-12-27 18:14:45 +0100

    Added algorisms schema

---
+++ src/main/java/BackTracking.java
@@ -0,0 +1,16 @@
+public class BackTracking {
+
+  /*  public void backTracking (Solution s){
+        if (bt(s)){
+            handleSolution(s);
+        }else{
+            for(int i = 0; i < option; i++){
+                if (promising(s, option)){
+                    s = addOption(option);
+                    backTracking(s);
+                    s = removeOption(option);
+                }
+            }
+        }
+    }*/
+}
---
+++ src/main/java/Greedy.java
@@ -0,0 +1,20 @@
+public class Greedy {
+   /*public Solution greedy(boolean found) {
+
+        Candidate[] candidates;
+        Solution s;
+        found = false;
+
+        //s = init solution
+        //candidates = init candidates
+        for (int  i = 0; i < candidates.length && !found; i++) {
+            if (is_feasible(s, candidates[i])) {
+                s = addSolution(s, candidates[i]);
+            }
+            if (is_solution(s)){
+                found = true;
+            }
+        }
+        return s;
+    }*/
+}
