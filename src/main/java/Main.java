73a12ef28e14e52665b6f04ca54cd302f750bb66
NeilTorrero <neil.torrero@students.salle.url.edu>
2019-02-25 14:22:56 +0100

    Revert "Menu creado"
    This reverts commit 922b98ba1a6c0c20bec82e53ecdd4834746ec6e4.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,57 +1,6 @@
-import BackTracking.BackTrackingDist.BackDist;
-import BackTracking.BackTrackingDist.BackDistFiabilidad;
-import BackTracking.BackTrackingServer.ServersDist;
-import BranchBound.BranchBoundDist.BranchboundDist;
-import BranchBound.BranchBoundDist.BranchboundFiabilidad;
-import BranchBound.BranchBoundServer.BranchboundServer;
-import Greedy.GreedyDist.GreedyDist;
-import Greedy.GreedyDist.GreedyFiable;
-import Greedy.GreedyServer.GreedyServer;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int algorismo = 0;
-        do {
-            Scanner scanner = new Scanner(System.in);
-            algorismo = scanner.nextInt();
-            switch (algorismo) {
-                case 1:
-                    //BackTracking Distancia
-                    BackDist.main(args);
-                    //BackTracking Distancia fiable
-                    BackDistFiabilidad.main(args);
-                    //BackTracking servidores
-                    ServersDist.main(args);
-                    break;
-                case 2:
-                    //Branch&Bound Distancia
-                    BranchboundDist.main(args);
-                    //Branch&Bound Distancia fiable
-                    BranchboundFiabilidad.main(args);
-                    //Branch&Bound servidores
-                    BranchboundServer.main(args);
-                    break;
-                case 3:
-                    //Greedy Distancia
-                    GreedyDist.main(args);
-                    //Greedy Distancia fiable
-                    GreedyFiable.main(args);
-                    //Greedy servidores
-                    GreedyServer.main(args);
-                    break;
-                case 4:
-                    //Greedy + BackTracking
-                    break;
-                case 5:
-                    //Greedy + Branch&Bound
-                    break;
-                default:
-                    System.out.println("Opcio incorrecte");
-                    break;
-            }
-        }while (algorismo == 0);
+
     }
 }
         /*
